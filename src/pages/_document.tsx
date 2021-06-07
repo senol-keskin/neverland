c4e27ccd9f17393888561f9c70025eeb3f63c71a
senol-ksk <senolk@lidyateknoloji.com>
2021-06-07 19:57:06 +0300

    add google tag manager

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,8 +12,25 @@ export default class Document extends NextDocument {
   render(): JSX.Element {
     return (
       <Html>
-        <Head />
+        <Head>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${process.env.GTM_KEY}');`,
+            }}
+          />
+        </Head>
         <body>
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.GTM_KEY}"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+            }}
+          />
+
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
           <NextScript />

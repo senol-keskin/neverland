0f5989fc9fafd14c24bf9dd00db9e5fb76c69a10
senol-ksk <senolk@lidyateknoloji.com>
2021-06-07 20:16:29 +0300

    gtm env settings as next_pulic

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -19,14 +19,14 @@ export default class Document extends NextDocument {
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','${process.env.GTM_KEY}');`,
+          })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_KEY}');`,
             }}
           />
         </Head>
         <body>
           <noscript
             dangerouslySetInnerHTML={{
-              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.GTM_KEY}"
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_KEY}"
             height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
             }}
           />

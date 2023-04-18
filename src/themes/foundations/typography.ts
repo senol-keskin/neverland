49ae546d9a762cc018b0dc73a18b6f00a25a3c33
senol keskin <senolk@lidyateknoloji.com>
2023-04-18 10:57:57 +0300

    fix; resolved ts errors

--- src/themes/foundations/typography.ts
+++ src/themes/foundations/typography.ts
@@ -2,8 +2,7 @@ import type { StyleConfig } from '@chakra-ui/theme-tools'
 
 const typography: StyleConfig['baseStyle'] = {
   fonts: {
-    body:
-      'Inter var,ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji',
+    body: 'Inter var,ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji',
   },
 }
 
